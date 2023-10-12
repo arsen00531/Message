fe37cc51d87ac69ec920939065a6e0fda52fc26c
Арсен <arsendag505@gmail.com>
2023-10-12 12:48:27 +0300

    comm

--- middlewares/check.js
+++ middlewares/check.js
@@ -1,4 +1,5 @@
 module.exports = (cooki, jwt) => {
+    if (cooki === undefined) return null
     let name = null
     cooki.split(';').forEach((value) => {
         const decoded = jwt.decode(value.split('=')[1])
